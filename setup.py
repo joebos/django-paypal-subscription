8af0b4fdb5a4a8e99c26992734285bb84ca0a14b
Mart van de Ven <m@type.hk>
2012-12-09 00:33:00 +0800

    Change package name to 'django-paypal-subscription'

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name="django-subscription",
+setup(name="django-paypal-subscription",
            version="0.1",
-           description="Subscriptions based web app using django-paypal",
-           author="SaaS kit",
-           author_email="admin@saaskit.org",
+           description="Subscriptions based web app using django-paypal. Based off original work by SAAS-Kit's Django-subscription",
+           author="Mart van de Ven",
+           author_email="m@type.hk",
            packages=find_packages(),
            include_package_data=True,
 )
