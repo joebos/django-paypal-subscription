0b70ad64ade8164dcb62ae907d48e83d98ad8204
Mart van de Ven <m@type.hk>
2012-12-09 00:49:14 +0800

    Updated Attribution

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(name="django-paypal-subscription",
            version="0.1",
-           description="Subscriptions based web app using django-paypal. Based off original work by SAAS-Kit's Django-subscription",
+           description="Subscriptions based web app using django-paypal. "
+           "Based off original work by Django-subscription's SAAS-Kit/zhaque",
            author="Mart van de Ven",
            author_email="m@type.hk",
            packages=find_packages(),
